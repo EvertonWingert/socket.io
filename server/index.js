7d6f97ce17d6545896fa320d9df7edb381e3d90e
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-06-29 19:42:18 -0300

    add nodemon

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 const io = new Server(3000, { cors: { origin: '*' } });
 
 io.on("connection", (socket) => {
-    console.log("a user connected");
+    console.log("front-end id: " + socket.id);
     io.emit("notification", { message: "hello" });
 });
 
