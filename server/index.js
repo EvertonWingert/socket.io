a7562102354656cce648c43054259c4db91c7ed4
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-06-29 19:47:40 -0300

    add push notification example

--- server/index.js
+++ server/index.js
@@ -2,10 +2,15 @@ import { client } from './service/socket.js'
 
 client.on("connection", (socket) => {
     console.log("front-end id: " + socket.id);
-    client.emit("notification", { message: "hello" });
 });
 
 
+
+//Simulate push notification
+setInterval(() => {
+    client.emit("notification", { title: "Nova mensagem de fulano", from: "Fulano" });
+}
+    , 2000);
 
 
 
